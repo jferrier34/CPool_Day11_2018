73816457b0e943a6dfd6ebab2b4767a524ebd66e
Julien Ferrier <jferrier@localhost.localdomain>
2018-10-16 13:10:27 +0200

    task_two

---
+++ my_list_size.c
@@ -0,0 +1,21 @@
+/*
+** EPITECH PROJECT, 2018
+** my_list_size.c
+** File description:
+** 
+*/
+
+#include "my.h"
+#include <stdlib.h>
+
+int my_list_size(linked_list_t const *begin)
+{
+    int nb_params;
+
+    nb_params = 0;
+    while (begin != NULL) {
+        begin = begin->next;
+        nb_params = nb_params + 1;
+    }
+    return (nb_params);
+}
